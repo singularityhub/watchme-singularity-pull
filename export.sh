b2ac7bd34251d34651f6cb76ec397e939e3de28e
Vanessa Sochat <vsochat@stanford.edu>
2019-05-18 11:42:23 -0400

    export needs to be json
    Signed-off-by: Vanessa Sochat <vsochat@stanford.edu>

--- export.sh
+++ export.sh
@@ -5,6 +5,6 @@ mkdir -p "export"
 
 for folder in $(watchme list singularity-pull); do
     if [[ "${folder}" == *"decorator"* ]]; then
-        watchme export singularity-pull $folder result.json --force --out export/$folder.json
+        watchme export singularity-pull $folder result.json --force --out export/$folder.json --json
     fi
 done
